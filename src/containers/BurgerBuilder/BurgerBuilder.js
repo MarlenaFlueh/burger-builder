e97ea8263c37ffdefcd8c5909093dc10528f687d
MarlenaFlueh <mf@esyoil.com>
2018-05-21 18:11:29 +0200

    add burger folder

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import Aut from "../../hoc/Aut";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Aut>
+        <div>Burger</div>
+        <div>Build Controls</div>
+      </Aut>
+    );
+  }
+}
+
+export default BurgerBuilder;
