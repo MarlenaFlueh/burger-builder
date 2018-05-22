ef6a6cbfa4994c6fbc4ce709140c1b82edb51217
MarlenaFlueh <mf@esyoil.com>
2018-05-22 16:36:15 +0200

    add dynamic ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,19 @@ import Aut from "../../hoc/Aut";
 import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      bacon: 1,
+      cheese: 1,
+      meat: 2,
+      salad: 2
+    }
+  };
+
   render() {
     return (
       <Aut>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Aut>
     );
