93d376c398d5b5197af3e8747c43e790bcb04e48
MarlenaFlueh <mf@esyoil.com>
2018-05-28 22:21:21 +0200

    add basic toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,11 @@ import React from "react";
 
 import Aut from "../../hoc/Aut";
 import classes from "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const layout = props => (
   <Aut>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
     <main className={classes.Content}>{props.children}</main>
   </Aut>
 );
