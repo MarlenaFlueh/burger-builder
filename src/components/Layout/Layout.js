deae01e31d2f3c0cffa91039a0ab09409c39f28e
MarlenaFlueh <mf@esyoil.com>
2018-05-30 00:09:01 +0200

    add responsive buttons

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,12 @@ import React from "react";
 import Aut from "../../hoc/Aut";
 import classes from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = props => (
   <Aut>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </Aut>
 );
