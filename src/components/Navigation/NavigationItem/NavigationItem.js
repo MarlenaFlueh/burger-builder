fa985c2bc8c6337fd0b8e50842c7705e2114f504
MarlenaFlueh <mf@esyoil.com>
2018-05-29 09:16:16 +0200

    add logo and first nav items

---
+++ src/components/Navigation/NavigationItem/NavigationItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NavigationItem = () => (
+  <li>
+    <a href="/">A Link</a>
+  </li>
+);
+
+export default NavigationItem;
---
+++ src/components/Navigation/NavigationItems/Navigationitems.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NavigationItem from "../NavigationItem/NavigationItem";
+
+const navigationItems = props => (
+  <ul>
+    <NavigationItem />
+  </ul>
+);
+
+export default navigationItems;
