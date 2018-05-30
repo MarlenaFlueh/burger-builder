328f9ad68658b592f9883361b9945b0166476753
MarlenaFlueh <mf@esyoil.com>
2018-05-30 19:44:19 +0200

    improve code with lifecycle method

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
 import classes from "./Modal.css";
 import Aut from "../../../hoc/Aut";
 import Backdrop from "../Backdrop/Backdrop";
 
-const modal = props => (
-  <Aut>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.show ? "1" : "0"
-      }}
-    >
-      {props.children}
-    </div>
-  </Aut>
-);
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.show !== nextProps.show;
+  }
 
-export default modal;
+  render() {
+    return (
+      <Aut>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0"
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aut>
+    );
+  }
+}
+
+export default Modal;
