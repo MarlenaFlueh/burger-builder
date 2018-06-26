ae6ea9769b8d7bf18985911e4cb1b6f9b765717b
MarlenaFlueh <mf@esyoil.com>
2018-06-26 09:03:23 +0200

    add dynamic input component

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -15,7 +15,7 @@ const order = props => {
         display: "inline-block",
         margin: "0 8px",
         border: "1px solid #ccc",
-        padding: "5 px"
+        padding: "5px"
       }}
       key={ingr.name}
     >
