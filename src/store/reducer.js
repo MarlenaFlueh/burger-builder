85371c6e94d6c9509fc14cc7348ce4222a4cd2de
MarlenaFlueh <mf@esyoil.com>
2018-07-02 21:29:49 +0200

    create add ingredient method

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,32 @@
+import * as actionTypes from "./actions";
+
 const initialState = {
+  ingredients: {
+    bacon: 0,
+    cheese: 0,
+    meat: 0,
+    salad: 0
+  },
+  totalPrice: 8
+};
 
-}
-
-const reducer = (state = initialState, action) => ();
+const reducer = (state = initialState, action) => {
+  switch (action.foodType) {
+    case actionTypes.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.foodType]: state.ingredients[action.foodType] + 1
+        }
+      };
+    case actionTypes.REMOVE_INGREDIENT:
+      return {
+        ...state
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
