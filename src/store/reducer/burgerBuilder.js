cf84fa6e560b31a8c73e2b3342520f05384ab878
MarlenaFlueh <mf@esyoil.com>
2018-07-11 20:32:20 +0200

    clean up switch case statement, find small bug - fix it later

--- src/store/reducer/burgerBuilder.js
+++ src/store/reducer/burgerBuilder.js
@@ -14,50 +14,65 @@ const ingredientPrices = {
   salad: 0.5
 };
 
+// in my opinion ugly to use the updateObject function
+const addIngredient = (state, action) => {
+  const updatedIngredient = {
+    [action.foodType]: state.ingredients[action.foodType] + 1
+  };
+  const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+  const updatedState = {
+    ingredients: updatedIngredients,
+    totalPrice: state.totalPrice + ingredientPrices[action.foodType]
+  };
+  return updateObject(state, updatedState);
+};
+
+// it's shorter without updateObject and it's less complicate to reads
+const removeIngredient = (state, action) => {
+  return {
+    ...state,
+    ingredients: {
+      ...state.ingredients,
+      [action.foodType]: state.ingredients[action.foodType] - 1
+    },
+    totalPrice: state.totalPrice - ingredientPrices[action.foodType]
+  };
+};
+
+const setIngredients = (state, action) => {
+  return {
+    ...state,
+    ingredients: {
+      salad: action.ingredients.salad,
+      bacon: action.ingredients.bacon,
+      cheese: action.ingredients.cheese,
+      meat: action.ingredients.meat
+    },
+    totalPrice: 4,
+    error: false
+  };
+};
+
+const fetchIngridientsFailed = state => {
+  return {
+    ...state,
+    error: true
+  };
+};
+
 const reducer = (state = initialState, action) => {
-  // in my opinion ugly to use the updateObject function
   switch (action.type) {
     case actionTypes.ADD_INGREDIENT:
-      const updatedIngredient = {
-        [action.foodType]: state.ingredients[action.foodType] + 1
-      };
-      const updatedIngredients = updateObject(
-        state.ingredients,
-        updatedIngredient
-      );
-      const updatedState = {
-        ingredients: updatedIngredients,
-        totalPrice: state.totalPrice + ingredientPrices[action.foodType]
-      };
-      return updateObject(state, updatedState);
+      return addIngredient(state, action);
 
-    // it's shorter without updateObject and it's less complicate to reads
     case actionTypes.REMOVE_INGREDIENT:
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [action.foodType]: state.ingredients[action.foodType] - 1
-        },
-        totalPrice: state.totalPrice - ingredientPrices[action.foodType]
-      };
+      return removeIngredient(state, action);
+
     case actionTypes.SET_INGREDIENTS:
-      return {
-        ...state,
-        ingredients: {
-          salad: action.ingredients.salad,
-          bacon: action.ingredients.bacon,
-          cheese: action.ingredients.cheese,
-          meat: action.ingredients.meat
-        },
-        totalPrice: 4,
-        error: false
-      };
+      return setIngredients(state, action);
+
     case actionTypes.FETCH_INGREDIENTS_FAILED:
-      return {
-        ...state,
-        error: true
-      };
+      return fetchIngridientsFailed(state);
     default:
       return state;
   }
