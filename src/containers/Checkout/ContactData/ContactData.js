15d1bc1b0cc54573cf72a4397c0238df32925d33
MarlenaFlueh <mf@esyoil.com>
2018-07-17 09:04:00 +0200

    create shared folder, share check validity mehtod

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,6 +8,7 @@ import classes from "./ContactData.css";
 import Input from "../../../components/UI/Input/Input";
 import withErrorHandler from "../../../hoc/withErrorHandler/withErrorHandler";
 import * as actionCreators from "../../../store/actions/index";
+import { checkValidity } from "../../../shared/utils";
 
 class ContactData extends Component {
   state = {
@@ -95,37 +96,6 @@ class ContactData extends Component {
     formIsValid: false
   };
 
-  checkValidity(value, rules) {
-    let isValid = true;
-    if (!rules) {
-      return true;
-    }
-
-    if (rules.required) {
-      isValid = value.trim() !== "" && isValid;
-    }
-
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid;
-    }
-
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid;
-    }
-
-    if (rules.isEmail) {
-      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-      isValid = pattern.test(value) && isValid;
-    }
-
-    if (rules.isNumeric) {
-      const pattern = /^\d+$/;
-      isValid = pattern.test(value) && isValid;
-    }
-
-    return isValid;
-  }
-
   orderHandler = event => {
     event.preventDefault();
 
@@ -148,7 +118,7 @@ class ContactData extends Component {
     const updatedOrderForm = { ...this.state.orderForm };
     const updatedFormItem = { ...updatedOrderForm[key] };
     updatedFormItem.value = event.target.value;
-    updatedFormItem.valid = this.checkValidity(
+    updatedFormItem.valid = checkValidity(
       updatedFormItem.value,
       updatedFormItem.validation
     );
