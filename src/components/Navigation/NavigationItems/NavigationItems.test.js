111fb6c0beb8920569c7fd92f33d66edb79d316d
MarlenaFlueh <mf@esyoil.com>
2018-07-18 08:08:33 +0200

    add more tests

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -8,8 +8,18 @@ import NavigationItem from "../NavigationItem/NavigationItem";
 configure({ adapter: new Adapter() });
 
 describe("NavigationItems", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
+
   it("should render two NavItem if the user is not authenticated", () => {
-    const wrapper = shallow(<NavigationItems />);
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+
+  it("should render three NavItem if the user is authenticated", () => {
+    wrapper.setProps({ isAuth: true });
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
 });
